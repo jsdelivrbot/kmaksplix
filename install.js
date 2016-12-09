0b3b91e0eac1e4538a8c0022970a10b08b0074b4
bobmcdouble3 <bobmcdouble3@gmail.com>
2016-12-08 19:47:05 -0500

    Add files via upload

---
+++ install.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Splix.io Mod By KMak
+// @description  It's lit bro
+// @version      99.9.999
+// @author       kmak
+// @match        http://splix.io
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @connect      splix.io
+// ==/UserScript==
+
+var sc = document.createElement("SCRIPT");
+sc.src = "https://raw.githubusercontent.com/bobmcdouble3/kmaksplix/master/kmaksplix.js";
+document.documentElement.appendChild(sc);
